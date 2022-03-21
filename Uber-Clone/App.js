f06cc885e6641003beb1fe775b72adf88844ddea
Ghanshyam Kumar Mahato <shyamghan17@gmail.com>
2022-03-21 18:51:52 +0545

    uber

--- Uber-Clone/App.js
+++ Uber-Clone/App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { store } from "./src/store";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Provider store={store}>
+ <View style={styles.container}>
+      <Text>lets build uber</Text>
       <StatusBar style="auto" />
     </View>
+    </Provider>
+   
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
